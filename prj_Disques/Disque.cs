0c22456bf574a73b31707087bba503b57c5af15c
Amine <amineamro500@gmail.com>
2021-03-29 00:46:56 -0700

    Example ordinateur OOP C#

---
+++ prj_Disques/Disque.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace prj_Disques
+{
+   public class Disque
+    {
+       //Les Attributs
+          //les Attributs d'objet
+             public  double Rayon;
+          //Les Attributs de Classe
+             public static   double PI;
+
+
+       //Les Méthodes
+          //les Méthodes d'objet
+             public double Surface()
+             {
+                 double s = PI * Rayon * Rayon;
+                 return s;
+             }
+             public double Perimetre()
+             {
+                 double p = 2 * PI * Rayon;
+                 return p;
+             }
+             public void Afficher()
+             {
+                 double su = Surface();
+                 double pe=Perimetre();
+                 Console.WriteLine("------------------------------");
+                 Console.WriteLine("Rayon du Disque :{0}", Rayon);
+                 Console.WriteLine("Surface du Disque :{0}", su);
+                 Console.WriteLine("Périmètre du Disque :{0}",pe);
+                 Console.WriteLine("------------------------------");
+             }
+        //Les Méthodes de Classe
+
+
+       //Les Constructeurs
+       public Disque()//Par Défaut
+       {       
+       }
+       public Disque(double ray)//Initialisation
+       {
+           Rayon = ray;
+       }
+    }
+}
---
+++ prj_Disques/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace prj_Disques
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Disque.PI = 3.14;
+
+            // Siasie le nombre des article 
+            Console.Write("Saisie le nombres des articles : ");
+            int nbDesArticles = int.Parse(Console.ReadLine());
+
+             // Declaration de tableau
+            Disque [] TabDisque = new Disque[nbDesArticles];
+
+
+            double ray;
+
+            for (int i = 0; i < TabDisque.Length; i++)
+            {
+                Console.Write("Saisie la valeur de rayon {0} : " , i+1);
+                ray = double.Parse(Console.ReadLine());
+
+
+                TabDisque[i] = new Disque(ray);
+            }
+
+            // Afichage 
+            for (int i = 0; i < TabDisque.Length; i++)
+            {
+                TabDisque[i].Afficher();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
